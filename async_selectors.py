c6ba2e7b9bf616f8a054d82ff34a0499647c0285
yevhen-m <yvhn.yvhn@gmail.com>
2016-05-15 18:18:49 +0300

    Make server reads and writes non-blocking

--- async_selectors.py
+++ async_selectors.py
@@ -1,15 +1,6 @@
 import socket
-import contextlib
 from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
 import collections
-
-
-@contextlib.contextmanager
-def close_socket(socket):
-    try:
-        yield
-    finally:
-        socket.close()
 
 
 class MyClientSocket:
@@ -32,9 +23,12 @@ class TCPServer:
         self.buffers = collections.defaultdict(list)
 
     def run(self):
+        '''
+        Start running TCPServer on its port.
+        '''
         server_sock = self._create_socket()
 
-        with close_socket(server_sock):
+        with server_sock:
             try:
                 client_sock, remote_addr = self.sock.accept()
             except BlockingIOError:
@@ -45,32 +39,64 @@ class TCPServer:
                 ready = self.selector.select()
                 for key, events in ready:
                     if key.fileobj is self.sock:
+                        # This is my server socket, so I accept incoming
+                        # connection
                         self._accept_client()
                         continue
 
                     client_sock = key.fileobj
                     if events == EVENT_READ:
+                        # Read data from client socket
                         self._read_request(client_sock)
+                        # If not all client data received, select again
                         if not client_sock.request:
                             continue
+                        # All client data received, so I can handle this
+                        # request
                         self._handle_received_request(client_sock)
-
                     elif events == EVENT_WRITE:
+                        # Now I can send response to the client
                         self._write_response(client_sock)
 
     def _handle_received_request(self, client_sock):
-        self.responses[client_sock] = client_sock.request.upper()
+        '''
+        Build client response and add it to self.responses dict and
+        register client_sock with selector for write events.
+        '''
+        # Response has to be larger than client socket recv buffer size
+        response = (client_sock.request.upper() * 100).encode('utf8')
+        self.responses[client_sock] = response
         self.selector.unregister(client_sock)
         self.selector.register(client_sock, EVENT_WRITE)
 
     def _write_response(self, client_sock):
-        response = self.responses[client_sock].encode('utf8')
-        client_sock.sendall(response)
+        '''
+        We can write data to client socket, so I get its response and
+        write it. I check how much bytes were sent and shorten response
+        accordingly. If all data is sent, I unregister socket and close it.
+        '''
+        client_name = self._get_client_name(client_sock)
+        while True:
+            response = self.responses[client_sock]
+            if not response:
+                # All response sent
+                break
+            try:
+                bytes_sent = client_sock.send(response)
+            except BlockingIOError:
+                return
+
+            print(bytes_sent, 'bytes sent to client', client_name)
+            self.responses[client_sock] = response[bytes_sent:]
+
+        print('Response sent to client', client_name)
         self.selector.unregister(client_sock)
         client_sock.close()
 
     def _create_socket(self):
         self.sock = socket.socket()
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('', self.port))
         self.sock.setblocking(False)
         self.sock.listen(10)
@@ -88,8 +114,12 @@ class TCPServer:
         return '{} {}'.format(*client_sock.getpeername())
 
     def _read_request(self, client_sock):
+        '''
+        I read client request in chunks and accumulate them in
+        self.buffers. When all data received, I set client_sock request
+        attribute value to the joined buffer data.
+        '''
         client_name = self._get_client_name(client_sock)
-        print('Reading from client', client_name)
 
         while True:
             try:
@@ -98,7 +128,8 @@ class TCPServer:
                 return
             print('Received chunk from client', client_name)
             if chunk.endswith(b'end'):
-                print('All data received from client', client_name)
+                self.buffers[client_sock].append(chunk)
+                print('Got all data from client', client_name)
                 request = b''.join(self.buffers[client_sock]).decode('utf8')
                 client_sock.request = request
             else:
--- client.py
+++ client.py
@@ -9,9 +9,12 @@ PORT = 8000
 def client():
     # Client sends chunks of data every half a second.
     sock = socket.socket()
+    # Decrease recv buffer size for this socket
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 0)  # 2304
     sock.connect(('localhost', PORT))
 
-    for i in range(1, 21):
+    num_of_chunks = 10
+    for i in range(1, num_of_chunks):
         sock.send(b'hello')
         print('Sent chunk', i)
         time.sleep(.5)
@@ -24,7 +27,9 @@ def client():
         # and join them when server closes the connection
         chunk = sock.recv(1024)
         if chunk:
+            print('Received chunk from', *sock.getpeername())
             buf.append(chunk)
+            time.sleep(.5)
         else:
             # server closed its connection
             response = b''.join(buf).decode('utf8')
