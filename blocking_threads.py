985c636cd84e7aa56036b9d1c9f05c3717473f16
yevhen-m <yvhn.yvhn@gmail.com>
2016-05-31 23:25:30 +0300

    Add blocking server on threads

---
+++ blocking_threads.py
@@ -0,0 +1,33 @@
+from concurrent.futures import ThreadPoolExecutor
+import socket
+import sys
+
+
+# Port as a second cli argument is optional
+port = int(sys.argv[-1]) if len(sys.argv) > 1 else 8000
+
+
+server_sock = socket.socket()
+server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_sock.bind(('', port))
+server_sock.listen(5)
+
+executor = ThreadPoolExecutor(3)
+
+
+def handle_client(client):
+    client_name = '%s %s' % client.getpeername()
+    print(client_name, 'incoming connection')
+    while True:
+        data = client.recv(1024).strip()
+        if not data:
+            break
+        print(client_name, data)
+    print(client_name, 'connection closed')
+    client.close()
+
+
+with server_sock:
+    while True:
+        client, addr = server_sock.accept()
+        executor.submit(handle_client, client)
